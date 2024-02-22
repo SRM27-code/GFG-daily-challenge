11016364601e2cbfed9966e8a305066ca12680eb
Shubham Mahind <158463288+SRM27-code@users.noreply.github.com>
2024-02-23 00:07:27 +0530

    Add files via upload

---
+++ 22 Feb (Distinct occurrences)/Distinct occurrences.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+
+class Solution
+{
+    public:
+    int subsequenceCount(string s, string t) {
+      //Your code here
+        int mod = 1000000007;
+        int n = s.length(), m = t.length();
+      
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+      
+        for (int i = 0; i <= n; ++i)
+            dp[i][0] = 1;
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                }else {
+                     dp[i][j] = dp[i - 1][j];
+                }
+            }
+        } 
+        
+        return dp[n][m] % mod;
+      
+    }
+};
