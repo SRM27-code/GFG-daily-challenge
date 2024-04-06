b28af4185910f5edf839870eae49690b462d8f9f
Shubham Mahind <158463288+SRM27-code@users.noreply.github.com>
2024-04-06 10:32:20 +0530

    Add files via upload

---
+++ April codes/6.Count ways to N'th Stair.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    long long countWays(int n) {
+        // your code here
+        
+        // function to count number of ways to react the nth stair
+        
+        return (long)(1+n/2);
+    }
+};
